7a416e59b220b5ff5a3882df661b472b784f70a9
ents <entsupml@gmail.com>
2018-05-06 20:30:48 +0300

    New debug version

--- gen.sh
+++ gen.sh
@@ -3,19 +3,20 @@
 
 set +e
 
-protoDir=protos
-
 # an: Dirty hack. Idea taken from https://github.com/grpc/grpc/blob/v1.10.x/src/php/bin/generate_proto_php.sh
 sed -i 's/message Empty /message EmptyMessage /g' ./vendor/google/protobuf/src/google/protobuf/empty.proto
 
-find $protoDir -name '*.proto' -exec sed -i 's/(google.protobuf.Empty)/(google.protobuf.EmptyMessage)/g' {} \;
+find protos -name '*.proto' -exec sed -i 's/(google.protobuf.Empty)/(google.protobuf.EmptyMessage)/g' {} \;
 
-find $protoDir -name '*.proto' -exec sed -i 's/\.protos/\.proto/g' {} \;
+find protos -name '*.proto' -exec sed -i 's/\.protos/\.proto/g' {} \;
 
-protoc --proto_path=$protoDir \
+protoc --proto_path=protos/finam/txsecurities/src/ \
+       --proto_path=protos/finam/transaq/src/ \
+       --proto_path=protos/finam/cgate/src/ \
+       --proto_path=protos/finam/proto/src/ \
        --proto_path=vendor/google/protobuf/src/ \
-       --php_out=. \
-       --grpc_out=. \
+       --php_out=auto \
+       --grpc_out=auto \
        --plugin=protoc-gen-grpc=/home/vagrant/grpc/bins/opt/grpc_php_plugin \
 	$@
 
