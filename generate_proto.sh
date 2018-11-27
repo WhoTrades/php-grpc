2b290caea645b32f1092af365e96266bb010d393
Anton Gorlanov <ag@whotrades.org>
2018-11-27 16:06:30 +0300

    [*] Php gRPC testing #WTA-1847

--- generate_proto.sh
+++ generate_proto.sh
@@ -4,7 +4,7 @@
 set +e
 
 # an: Dirty hack. Idea taken from https://github.com/grpc/grpc/blob/v1.10.x/src/php/bin/generate_proto_php.sh
-sed -i 's/message Empty /message EmptyMessage /g' ../../vendor/google/protobuf/src/google/protobuf/empty.proto
+sed -i 's/message Empty /message EmptyMessage /g' ../../google/protobuf/src/google/protobuf/empty.proto
 
 find protos -name '*.proto' -exec sed -i 's/(google.protobuf.Empty)/(google.protobuf.EmptyMessage)/g' {} \;
 
